afe47f39c89c19233341051270546b4e906f606f
Matheus Rodrigues de Oliveira <matheusrdeoliv1@gmail.com>
2026-09-17 19:44:01 -0300

    update

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -7,7 +7,7 @@ const ThemeContext = createContext<Ctx | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [theme, setTheme] = useState<Theme>(() =>
-    (localStorage.getItem("theme") as Theme) ?? "light"
+    (localStorage.getItem("theme") as Theme) ?? "dark"
   );
 
   useEffect(() => {
